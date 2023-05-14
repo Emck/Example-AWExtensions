5f1adcd213e7cd421af32550a17c48349c91459c
Emck <emck.do@gmail.com>
2023-05-14 14:18:35 +0800

    create a new project by macOS

---
+++ ExampleAWExtensions/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ExampleAWExtensions
+//
+//  Created by Emck on 2023/5/14.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ExampleAWExtensions/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ExampleAWExtensions
+//
+//  Created by Emck on 2023/5/14.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
