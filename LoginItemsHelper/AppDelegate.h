f2d08c58b7e6c68b61446b73cdce9c758f6af23a
Emck <emck.do@gmail.com>
2023-05-18 20:18:36 +0800

    add LoginItemsHelper Target

---
+++ LoginItemsHelper/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LoginItemsHelper
+//
+//  Created by Emck on 2023/5/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
